d689256030c0d0220490710a8b04fcdd4beb61e0
beata.krasnopolska@gmail.com <beata.krasnopolska@gmail.com>
2019-07-14 12:02:05 +0200

    Add project files.

---
+++ DateYesterday/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateYesterday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program calculates what date was yesterday");
+
+            Console.WriteLine("Today is: {0}", DateTime.Today.ToString("d"));
+
+            DateTime yesterday = YdCounter.GetYesterday();
+
+            Console.WriteLine("Yesterday was : {0}", yesterday.ToString("d"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DateYesterday/YdCounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateYesterday
+{
+    public static class YdCounter
+    {
+        public static DateTime GetYesterday()
+        {
+            return DateTime.Today.AddDays(-1);
+        }
+    }
+}
